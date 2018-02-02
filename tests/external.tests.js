b6ea5fcaf2e20942e0316677195db5767e32e024
Eemeli Aro <eemeli@gmail.com>
2018-02-03 01:08:32 +0200

    Move node-properties-parser tests to their own folder

--- tests/external.tests.js
+++ tests/external.tests.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 const path = require('path')
-const { parse, parseLine, stringify } = require('../index')
-
-const root = path.resolve(__dirname, '@js.properties')
-const tests = fs.readdirSync(root).filter(name => /\.properties$/.test(name))
+const { parse, stringify } = require('../index')
 
 describe('@js.properties', () => {
+  const root = path.resolve(__dirname, '@js.properties')
+  const tests = fs.readdirSync(root).filter(name => /\.properties$/.test(name))
+
   tests.forEach(name => {
     test(name, () => {
       const src = fs.readFileSync(path.resolve(root, name), 'utf8')
@@ -18,6 +18,7 @@ describe('@js.properties', () => {
       expect(res2).toMatchObject(exp)
     })
   })
+
   test('namespaced properties with path', () => {
     const src = fs.readFileSync(path.resolve(root, 'namespaced.properties'), 'utf8')
     const tgt = fs.readFileSync(path.resolve(root, 'namespaced.properties.namespaced.json'), 'utf8')
@@ -30,42 +31,12 @@ describe('@js.properties', () => {
   })
 })
 
-// This test has been extracted from the node-properties-parser project by Xavi Ramirez, at:
-// https://github.com/xavi-/node-properties-parser/tree/fb1b7038380fa295ff80ed0d1a1fad3ad1788738/test
-// It has been made available under the MIT license
 describe('node-properties-parser', () => {
+  const root = path.resolve(__dirname, 'node-properties-parser')
   test('node-properties-parser', () => {
-    const src =
-`# You are reading the ".properties" entry.
-! The exclamation mark can also mark text as comments.
-lala=\\u210A the foo foo \\
-                    lalala;
-website = http://en.wikipedia.org/
-language = English
-# The backslash below tells the application to continue reading
-# the value onto the next line.
-message = Welcome to \\
-          Wikipedia!
-# Add spaces to the key
-key\\ with\\ spaces = This is the value that could be looked up with the key "key with spaces".
-# Unicode
-tab : \\u0009
-long-unicode : \\u00000009
-space\\ separator     key val \\n three
-another-test ::: hihi
-   null-prop`
-    const exp = {
-      lala: 'ℊ the foo foo lalala;',
-      website: 'http://en.wikipedia.org/',
-      language: 'English',
-      message: 'Welcome to Wikipedia!',
-      'key with spaces': 'This is the value that could be looked up with the key "key with spaces".',
-      tab: '\t',
-      'long-unicode': '\u00000009', // sic
-      'space separator': 'key val \n three',
-      'another-test': ':: hihi',
-      'null-prop': ''
-    }
+    const src = fs.readFileSync(path.resolve(root, 'test.properties'), 'utf8')
+    const tgt = fs.readFileSync(path.resolve(root, 'test.json'), 'utf8')
+    const exp = JSON.parse(tgt)
     const res = parse(src)
     expect(res).toMatchObject(exp)
     const src2 = stringify(res)
---
+++ tests/node-properties-parser/README.md
@@ -0,0 +1,6 @@
+The test case in this folder has been extracted from the
+**node-properties-parser** project by Xavi Ramirez, at the following URL:
+
+https://github.com/xavi-/node-properties-parser/tree/fb1b7038380fa295ff80ed0d1a1fad3ad1788738/test
+
+These files have been available under the MIT license.
